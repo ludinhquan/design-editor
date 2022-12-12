9c8f4f0164046b00f0aba6f6c92f46232e85aeda
ludinhquan <ludinhquan@gmail.com>
2022-12-12 09:03:31 +0700

    fix: update ant css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './app'
-import 'antd/dist/antd.css';
+import 'antd/dist/reset.css';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
