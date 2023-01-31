1af4fa07df431a37271ecb603354e156f81a0759
ludinhquan <ludinhquan@gmail.com>
2023-01-31 20:49:30 +0700

    json handler

--- src/core/Handler/Handler.ts
+++ src/core/Handler/Handler.ts
@@ -6,26 +6,30 @@ import {BaseHandler} from "./BaseHandler";
 import {ElementHandler} from "./ElementHandler";
 import {KeyboardHandler} from "./KeyboardHandler";
 import {EventHandler} from "./EventHandler";
+import {JsonHandler} from "./JsonHandler";
 
 export class Handler {
   public readonly elementHandler: ElementHandler;
   public readonly eventHandler: EventHandler;
   public readonly actionHandler: ActionHandler;
   public readonly keyboardHandler: KeyboardHandler;
+  public readonly jsonHandler: JsonHandler;
 
-  private readonly handlers: BaseHandler[]
+  private readonly handlers: BaseHandler[];
 
   constructor(public canvasInstance: CanvasInstance) {
     this.elementHandler = new ElementHandler(this);
     this.eventHandler = new EventHandler(this);
     this.actionHandler = new ActionHandler(this);
     this.keyboardHandler = new KeyboardHandler(this);
+    this.jsonHandler = new JsonHandler(this);
 
     this.handlers = [
       this.elementHandler,
       this.eventHandler,
       this.actionHandler,
-      this.keyboardHandler
+      this.keyboardHandler,
+      this.jsonHandler
     ]
   }
 
---
+++ src/core/Handler/JsonHandler.ts
@@ -0,0 +1,36 @@
+import {fabric} from "fabric";
+import {nanoid} from "nanoid";
+import {BaseHandler} from "./BaseHandler";
+import {Handler} from "./Handler";
+
+type RawData = {
+  type: string
+}
+
+export class JsonHandler extends BaseHandler {
+  constructor(handler: Handler) {
+    super(handler)
+    this.registerHandlers();
+  }
+
+  private registerHandlers = () => {
+    this.canvas.on('object:modified', this.onObjectModified.bind(this));
+  }
+
+  private onObjectModified() {
+    const jsonData = this.jsonToRawData(this.canvas.getObjects());
+    console.log(this.canvas.toJSON())
+    console.log(JSON.stringify(jsonData, null, 2));
+  }
+
+  public jsonToRawData(objects: fabric.Object[], groupIds: string[] = []) {
+    const rawData: RawData[] = objects.flatMap(object => {
+      if (object.type === 'group') return this.jsonToRawData((object as fabric.Group).getObjects(), [nanoid(), ...groupIds]);
+      return {type: object.type, groupIds, x: object.left, y: object.top, text: (object as fabric.Textbox).text}
+    });
+    return rawData;
+  }
+
+  public rawDataToJson(rawData: RawData[]) {
+  }
+}
